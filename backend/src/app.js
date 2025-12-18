e71434e55251f12597d5c8cc9869b86fcec1daef
Chandan Chaudhary <166887599+chandan9648@users.noreply.github.com>
2025-12-18 15:37:19 +0530

    Fix CORS origin for production environment

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 // Configure CORS for dev front-end
 app.use(cors({
-	origin: ["RecipeeBox.vercel.app", "http://localhost:5173"],
+	origin: ["recipeebox.vercel.app", "http://localhost:5173"],
 	credentials: true
 }));
 
